0984f9e335b6312d64e589c9ed889209a418e25d
Anne-So <Anne-So@172.16.125.94>
2014-01-14 11:02:58 +0100

    creation main

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -1,14 +1,23 @@
 package fr.gphy.m2.keuv.tpforma;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-public class PDBUrlBuilder 
-{
-	public static URL getFirstPdbUrl(String uniprotId) throws IOException 
-	{
-		// TODO: gérer le proxy
-		URL url = new URL("http://www.uniprot.org/uniprot/" + uniprotId +".txt");
-		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-		// TODO: lire le fichier
-		return null;
+public class PDBUrlBuilder {
+
+    public static URL getFirstPdbUrl(String uniprotId) throws IOException {
+
+        // TODO: gérer le proxy
+        URL url = new URL("http://www.uniprot.org/uniprot/"+uniprotId + ".txt");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                
+		
+        // TODO: lire le fichier
+	return null;
+    
+
 	}
 }
