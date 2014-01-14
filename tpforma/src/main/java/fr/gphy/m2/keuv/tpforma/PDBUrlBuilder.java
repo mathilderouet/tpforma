6b4080d1db7438dd748248fe1604fb627c41df06
jpeyraud <Altarrys@Jeremy>
2014-01-14 10:13:24 +0100

    new branch

---
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -0,0 +1,14 @@
+package fr.gphy.m2.keuv.tpforma;
+
+
+public class PDBUrlBuilder 
+{
+	public static URL getFirstPdbUrl(String uniprotId) throws IOException 
+	{
+		// TODO: gérer le proxy
+		URL url = new URL("http://www.uniprot.org/uniprot/" + uniprotId +".txt");
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+		// TODO: lire le fichier
+		return null;
+	}
+}
